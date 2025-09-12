5e716d11c2f497d1434af8b4b7d685274ef96c74
FedyBenMahfoudh <fedi.benmahfoudh@isimm.u-monastir.tn>
2025-09-12 23:46:29 +0100

    Set up Neon Postgre w/ Drizzle

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import express from 'express';
 const app = express();
 
-const port = process.env.PORT || 3000;
-
 app.get('/', (req, res) => {
   res.status(200).send('Hello from Acquisitions API');
 });
